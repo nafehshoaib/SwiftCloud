4a56cdc1e3a149b5636a85119938960426e9c05c
Nafeh Shoaib <nafehshoaib14@gmail.com>
2023-11-22 12:28:15 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,9 +6,9 @@ import PackageDescription
 let package = Package(
     name: "SwiftCloud",
     platforms: [
-        .iOS(.v15),
-        .watchOS(.v8),
-        .macOS(.v12)
+        .iOS(.v16),
+        .watchOS(.v9),
+        .macOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
