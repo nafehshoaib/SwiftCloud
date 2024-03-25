091367818ffbf0a45ddfcab1c9756db7252e153f
Nafeh Shoaib <nafehshoaib14@gmail.com>
2024-03-25 14:31:55 -0400

    Added cURL generator for URLRequest

---
+++ Sources/SwiftCloud/URLRequest+Method+cURL.swift
@@ -0,0 +1,48 @@
+//
+//  URLRequest+HTTPMethod.swift
+//
+//
+//  Created by Nafeh Shoaib on 8/16/21.
+//
+
+import Foundation
+
+extension URLRequest {
+    public enum HTTPMethod: String {
+        case put = "PUT"
+        case post = "POST"
+        case get = "GET"
+        case delete = "DELETE"
+        case head = "HEAD"
+    }
+    
+    func cURL() -> String {
+        let cURL = "curl -f"
+        let method = "-X \(self.httpMethod ?? "GET")"
+        let url = url.flatMap { "--url '\($0.absoluteString)'" }
+        
+        let header = self.allHTTPHeaderFields?
+            .map { "-H '\($0): \($1)'" }
+            .joined(separator: " ")
+        
+        let data: String?
+        if let httpBody, !httpBody.isEmpty {
+            if let bodyString = String(data: httpBody, encoding: .utf8) { // json and plain text
+                let escaped = bodyString
+                    .replacingOccurrences(of: "'", with: "'\\''")
+                data = "--data '\(escaped)'"
+            } else { // Binary data
+                let hexString = httpBody
+                    .map { String(format: "%02X", $0) }
+                    .joined()
+                data = #"--data "$(echo '\#(hexString)' | xxd -p -r)""#
+            }
+        } else {
+            data = nil
+        }
+        
+        return [cURL, method, url, header, data]
+            .compactMap { $0 }
+            .joined(separator: " ")
+    }
+}
